ca2a62a0aafa1ade56d1f86accdedca0214be394
Bob Aubouin <bob.aubouin-pairault@univ-grenoble-alpes.fr>
2022-09-01 12:03:53 +0200

    new regressor

--- Regression.py
+++ Regression.py
@@ -16,7 +16,7 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.decomposition import PCA
 from sklearn.svm import SVR
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet, TheilSenRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
 from metrics_functions import compute_metrics, plot_results
@@ -53,7 +53,7 @@ Patients_test = Patients_test[Patients_test['full']==0]
 
 #%% Model based Regressions
 
-feature = 'A'
+feature = 'B'
 #feat_A
 if feature=='A':
     X_col = ['age', 'sex', 'height', 'weight', 'bmi', 'lbm', 'MAP_base_case', 'Ce_Prop', 'Ce_Rem', 'Ce_Prop_MAP', 'Ce_Rem_MAP']
@@ -64,7 +64,9 @@ Patients_train = Patients_train[X_col + ['caseid','BIS','MAP','train_set']].drop
 Patients_test = Patients_test[X_col + ['caseid','BIS','MAP']].dropna()
 
 
-name_rg = 'MLPRegressor'
+name_rg = 'TheilSenRegressor'
+poly_degree = 1
+pca_bool = False
 regressors = {}
 
 
@@ -86,9 +88,7 @@ for y_col in ['BIS', 'MAP']: #
         
     except: #Otherwhise train the regressors and save it
         Y_train = Patients_train[y_col]
-        if name_rg=='ElasticNet':
-            poly_degree = 1
-            pca_bool = False
+        if name_rg=='ElasticNet' or name_rg=='TheilSenRegressor':
             X_train = PolynomialFeatures(degree=poly_degree, include_bias=False).fit_transform(Patients_train[X_col].values)   
             scaler = StandardScaler()
             X_train = scaler.fit_transform(X_train) 
@@ -130,9 +130,13 @@ for y_col in ['BIS', 'MAP']: #
         
         elif name_rg=='MLPRegressor':
             rg = MLPRegressor(learning_rate = 'adaptive', max_iter = 1000)
-            Gridsearch = GridSearchCV(rg, {'hidden_layer_sizes': [64, 128, 256, 512], 'alpha': np.logspace(-4,-2,3),
-                                           'activation': ('tanh','relu')})
+            Gridsearch = GridSearchCV(rg, {'hidden_layer_sizes': [128, 256, 512], 'alpha': np.logspace(-4,-2,3),
+                                           'activation': ('tanh','relu','logistic','identity')})
             Gridsearch.fit(X_train, Y_train/100)
+        elif name_rg=='TheilSenRegressor':
+            rg = TheilSenRegressor(n_jobs=8)
+            Gridsearch = GridSearchCV(rg, {'max_subpopulation': [1e4, 1e5]})
+            Gridsearch.fit(X_train, Y_train)
             
         print(Gridsearch.best_params_)
         
@@ -142,7 +146,7 @@ for y_col in ['BIS', 'MAP']: #
     
     #--------------test performances on test cases-------------
     
-    if name_rg=='ElasticNet':
+    if name_rg=='ElasticNet' or name_rg=='TheilSenRegressor':
         X_train = PolynomialFeatures(degree=poly_degree, include_bias=False).fit_transform(Patients_train[X_col].values)
         scaler = StandardScaler()
         scaler.fit(X_train) 
@@ -186,7 +190,7 @@ for y_col in ['BIS', 'MAP']: #
         Test_data['pred_MAP'] = y_predicted  
     
     #-----------------test performances on train cases--------------------
-    if name_rg=='ElasticNet':
+    if name_rg=='ElasticNet' or name_rg=='TheilSenRegressor':
         X_train = PolynomialFeatures(degree=poly_degree, include_bias=False).fit_transform(Patients_train[X_col].values)
         X_train = scaler.transform(X_train)
         if pca_bool:
